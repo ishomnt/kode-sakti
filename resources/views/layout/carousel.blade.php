1af26fee05e7336feb26dcbd9f6731c25ca92f27
dimasssav12 <ajarehaba@gmail.com>
2024-09-16 19:57:02 +0700

    update carousel

--- resources/views/layout/carousel.blade.php
+++ resources/views/layout/carousel.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link rel="stylesheet" href="{{ asset('css/carousel.css') }}">
 </head>
 
@@ -79,11 +80,20 @@
             </div>
         </div>
         <div class="jenjang-container">
-            <button class="jenjang-btn fs">VIDEO TUTORIAL</button>
-            <button class="jenjang-btn">MATERI</button>
-            <button class="jenjang-btn">LATIHAN SOAL</button>
-            <button class="jenjang-btn">KOMUNITAS</button>
+            <button class="jenjang-btn fs">
+                <i class="fas fa-video"></i> VIDEO TUTORIAL
+            </button>
+            <button class="jenjang-btn">
+                <i class="fas fa-book"></i> MATERI
+            </button>
+            <button class="jenjang-btn">
+                <i class="fas fa-pencil-alt"></i> LATIHAN SOAL
+            </button>
+            <button class="jenjang-btn">
+                <i class="fas fa-users"></i> KOMUNITAS
+            </button>
         </div>
+
     </div>
 
 
