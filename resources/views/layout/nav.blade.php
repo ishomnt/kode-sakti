7bff6642bb3d631cea9194b0f0ed935b53427383
IshomNT <ishomnurtriadi111@gmail.com>
2024-09-16 10:23:28 +0700

    Revisi navbar

--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -1,43 +1,22 @@
 <header>
-    <div class="navbar">
-        <div class="logo">
-            <a href="{{ asset('/') }}"><img src="{{ asset('image/logoweb.jpg') }}" alt="Logo Web" /></a>
-        </div>
-        <ul class="links">
-            <li><a class="link" href="{{ asset('/') }}">Beranda</a></li>
-            <li><a class="link" href="{{ asset('/') }}">Beranda</a></li>
-            <li><a class="link" href="{{ asset('/') }}">Beranda</a></li>
-            <li><a class="link" href="{{ asset('/') }}">Beranda</a></li>
-            <li><a class="link" href="{{ asset('/') }}">Beranda</a></li>
-        </ul>
-        <a href="#" class="action_btn">Mulai</a>
-        <div class="toggle_btn">
-            <i class="fa-solid fa-bars"></i>
-        </div>
+    <nav class="navbar">
+      <div class="logo">< BeCo /></div>
+      <ul class="nav-items">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">FAQ</a></li>
+      </ul>
+      <div class="hamburger">
+        <i class="fas fa-bars"></i>
+      </div>
+    </nav>
+    <div class="mobile-menu">
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">FAQ</a></li>
+      </ul>
     </div>
-
-    <div class="dropdown_menu">
-        <li><a class="link" href="{{ asset('/') }}">Beranda</a></li>
-        <li><a class="link" href="{{ asset('/') }}">Beranda</a></li>
-        <li><a class="link" href="{{ asset('/') }}">Beranda</a></li>
-        <li><a class="link" href="{{ asset('/') }}">Beranda</a></li>
-        <li><a class="link" href="{{ asset('/') }}">Beranda</a></li>
-        <li><a href="#" class="action_btn">Mulai</a></li>
-    </div>
-</header>
-
-<script>
-    const toggleBtn = document.querySelector('.toggle_btn');
-    const toggleBtnIcon = document.querySelector('.toggle_btn i');
-    const dropDownMenu = document.querySelector('.dropdown_menu');
-
-    toggleBtn.onclick = function() {
-        dropDownMenu.classList.toggle('open');
-        const isOpen = dropDownMenu.classList.contains('open');
-
-
-        toggleBtnIcon.classList = isOpen
-            ? 'fa-solid fa-xmark'
-            : 'fa-solid fa-bars';
-    }
-</script>
+  </header>
