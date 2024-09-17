f6b6a2083646af0c30e7a97766428174bded37b3
tinodoang <tinomare245@gmail.com>
2024-09-17 14:55:02 +0700

    tonoonit

--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -1,22 +1,22 @@
 <header>
     <nav class="navbar">
-      <a href="" class="logo"><img src="{{ asset('image/Logo BeCo.png') }}" class="logo-img" alt=""></a>
-      <ul class="nav-items">
-        <li><a href="#">Beranda</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Services</a></li>
-        <li><a href="#">FAQ</a></li>
-      </ul>
-      <div class="hamburger">
-        <i class="fas fa-bars"></i>
-      </div>
+        <a href="" class="logo"><img src="{{ asset('image/Logo BeCo.png') }}" class="logo-img" alt=""></a>
+        <ul class="nav-items">
+            <li><a href="#">Beranda</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">FAQ</a></li>
+        </ul>
+        <div class="hamburger">
+            <i class="fas fa-bars"></i>
+        </div>
     </nav>
     <div class="mobile-menu">
-      <ul>
-        <li><a href="#">Beranda</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Services</a></li>
-        <li><a href="#">FAQ</a></li>
-      </ul>
+        <ul>
+            <li><a href="#">Beranda</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">FAQ</a></li>
+        </ul>
     </div>
-  </header>
+</header>
--- resources/views/master/index.blade.php
+++ resources/views/master/index.blade.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ asset('css/font.css') }}">
     <link rel="stylesheet" href="{{ asset('css/nav.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/card.css') }}">
     <link rel="stylesheet" href="{{ asset('css/carousel.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
     <title>Document</title>
@@ -15,6 +16,7 @@
 <body>
     @include('layout.nav')
     @include('layout.carousel')
+    @include('layout.card')
     
     <div class="container mt-4">
         @yield('container')
@@ -22,6 +24,36 @@
 
 
     <script src="{{ asset('js/carousel.js') }}"></script>
+    <script>
+        const hamburger = document.querySelector('.hamburger');
+        const mobileMenu = document.querySelector('.mobile-menu');
+        const icon = hamburger.querySelector('i');
+
+        hamburger.addEventListener('click', () => {
+            mobileMenu.classList.toggle('active');
+
+            // Ganti ikon antara hamburger dan X
+            if (mobileMenu.classList.contains('active')) {
+                icon.classList.remove('fa-bars');
+                icon.classList.add('fa-times');
+            } else {
+                icon.classList.remove('fa-times');
+                icon.classList.add('fa-bars');
+            }
+        });
+        s
+    </script>
+
+    <script>
+        window.addEventListener('scroll', function() {
+            const header = document.querySelector('header');
+            if (window.scrollY > 50) { // Adjust the value as needed
+                header.classList.add('scrolled');
+            } else {
+                header.classList.remove('scrolled');
+            }
+        });
+    </script>
 
 </body>
 
