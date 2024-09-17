7ba4ad7bfafe037ecacfb77784f9c82b49619ae3
dimasssav12 <ajarehaba@gmail.com>
2024-09-17 08:35:40 +0700

    update carousel

--- resources/views/layout/carousel.blade.php
+++ resources/views/layout/carousel.blade.php
@@ -4,15 +4,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link rel="stylesheet" href="{{ asset('css/carousel.css') }}">
 </head>
 
 <body>
-
-    <!-- slider -->
-
     <div class="slider">
         <!-- list Items -->
         <div class="list">
@@ -53,8 +51,9 @@
 
         <!-- button arrows -->
         <div class="arrows">
-            <button id="prev"><</button>
-            <button id="next">></button>
+            <button id="prev">
+                << /button>
+                    <button id="next">></button>
         </div>
         <!-- thumbnail -->
         <div class="thumbnail-wrapper">
@@ -93,12 +92,7 @@
                 <i class="fas fa-users"></i> KOMUNITAS
             </button>
         </div>
-
     </div>
-
-
-
     <script src="{{ asset('js/carousel.js') }}"></script>
 </body>
-
 </html>
