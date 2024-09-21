ebfc85cac08e19ac3917cf77da6e628a0afdf404
IshomNT <ishomnurtriadi111@gmail.com>
2024-09-21 10:58:23 +0700

    Revisi layout & revisi script

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('master.index');
-});
+})->name('main');
 
+Route::prefix('/materi')->group(function(){
+    Route::get('/video-html-css', function(){return view('pages.videos.lihatvideohtmlcss');})->name('html-css');
+    Route::get('/video-javascript', function(){return view('pages.videos.lihatvideoJs');})->name('javascript');
+    Route::get('/video-php', function(){return view('pages.videos.lihatvideoPhp');})->name('php');
+    Route::get('/video-laravel', function(){return view('pages.videos.lihatvideoLaravel');})->name('laravel');
+});
