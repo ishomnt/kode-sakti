3024f86130031582372be711aaa86989fc0a24ca
IshomNT <ishomnurtriadi111@gmail.com>
2024-09-28 18:18:46 +0700

    Tambahan route untuk komunitas

--- resources/views/komunitas/index.blade.php
+++ resources/views/komunitas/index.blade.php
@@ -8,36 +8,38 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="{{ asset('css/komunitas.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/nav-pages.css') }}">
     <style>
         .box-wrapper {
             width: 100%;
-            margin-top: 20px;
+            margin-top: 60px;
         }
-    
+
         .title {
             text-align: center;
             margin-bottom: 20px;
         }
-    
+
         .popular-title {
             font-weight: bold;
             color: #000;
         }
-    
+
         .desc-title {
             color: #9ba8b7;
         }
-    
+
         .desc {
             justify-content: center;
             align-items: center;
             gap: 400px;
         }
     </style>
-    
+
 </head>
 
 <body>
+    @include('layout.navpage')
     <div class="box-wrapper">
         <div class="title">
             <h2 class="popular-title">Komunitas</h2>
@@ -51,8 +53,8 @@
     <div class="container">
         <div class="tutorial-header">
             <div class="tutorial-text">
-                <h1>Membenarkan Error "Undefined Variable" Di PHP</h1>
-                <p>Error "Undefined Variable" di PHP terjadi ketika sebuah variabel digunakan tanpa dideklarasikan atau diinisialisasi sebelumnya.</p>
+                <a class="nav-link fs-5" href="{{ route('isi-komunitas', ['komunitas' => 'komunitas-php']) }}">Error "Undefined Variable" Di PHP</a>
+                <p>Error "Undefined Variable" ketika saya menjalankan pada web padahal sudah sesuai.</p>
                 <div class="d-flex">
                     <div class="tutorial-item">
                         PHP
@@ -67,9 +69,8 @@
     <div class="container">
         <div class="tutorial-header">
             <div class="tutorial-text">
-                <h1>Tutorial HTML untuk Pemula</h1>
-                <p>Belajar HTML dasar dari Nol. Pahami tag-tag esensial yang sering dipakai. Lalu buat sendiri websitemu
-                    dengan HTML. Selamat Belajar!</p>
+                <a class="nav-link fs-5" href="{{ route('isi-komunitas', ['komunitas' => 'komunitas-css']) }}">Mengapa saya tidak bisa mengubah warna latar belakang dengan CSS?</a>
+                <p>Saya sudah menambahkan background-color pada css saya tetapi tidak muncul setelah saya refresh</p>
                 <div class="d-flex">
                     <div class="tutorial-item">
                         HTML
@@ -84,9 +85,8 @@
     <div class="container">
         <div class="tutorial-header">
             <div class="tutorial-text">
-                <h1>Tutorial HTML untuk Pemula</h1>
-                <p>Belajar HTML dasar dari Nol. Pahami tag-tag esensial yang sering dipakai. Lalu buat sendiri websitemu
-                    dengan HTML. Selamat Belajar!</p>
+                <a class="nav-link fs-5" href="{{ route('isi-komunitas', ['komunitas' => 'komunitas-js']) }}">Mengapa saya mendapatkan error "TypeError: undefined is not a function" di JavaScript?</a>
+                <p>Saya mendapatkan pesan error "TypeError: undefined is not a function" pada console padahal saya sudah membuat function dan menuliskan dengan benar</p>
                 <div class="d-flex">
                     <div class="tutorial-item">
                         HTML
@@ -101,9 +101,8 @@
     <div class="container">
         <div class="tutorial-header">
             <div class="tutorial-text">
-                <h1>Tutorial HTML untuk Pemula</h1>
-                <p>Belajar HTML dasar dari Nol. Pahami tag-tag esensial yang sering dipakai. Lalu buat sendiri websitemu
-                    dengan HTML. Selamat Belajar!</p>
+                <a class="nav-link fs-5" href="{{ route('isi-komunitas', ['komunitas' => 'komunitas-js']) }}">Mengapa saya mendapatkan error "TypeError: undefined is not a function" di JavaScript?</a>
+                <p>Saya mendapatkan pesan error "TypeError: undefined is not a function" pada console padahal saya sudah membuat function dan menuliskan dengan benar</p>
                 <div class="d-flex">
                     <div class="tutorial-item">
                         HTML
@@ -115,23 +114,34 @@
             </div>
         </div>
     </div>
-    <div class="container">
-        <div class="tutorial-header">
-            <div class="tutorial-text">
-                <h1>Tutorial HTML untuk Pemula</h1>
-                <p>Belajar HTML dasar dari Nol. Pahami tag-tag esensial yang sering dipakai. Lalu buat sendiri websitemu
-                    dengan HTML. Selamat Belajar!</p>
-                <div class="d-flex">
-                    <div class="tutorial-item">
-                        HTML
-                    </div>
-                    <div class="tutorial-item">
-                        HTML Dasar
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+
+    <script>
+        const hamburger = document.querySelector('.custom-hamburger');
+        const mobileMenu = document.querySelector('.custom-mobile-menu');
+        const icon = hamburger.querySelector('i');
+
+        hamburger.addEventListener('click', () => {
+            mobileMenu.classList.toggle('active');
+
+            // Ganti ikon antara hamburger dan X
+            if (mobileMenu.classList.contains('active')) {
+                icon.classList.remove('fa-bars');
+                icon.classList.add('fa-times');
+            } else {
+                icon.classList.remove('fa-times');
+                icon.classList.add('fa-bars');
+            }
+        });
+
+        window.addEventListener('scroll', function() {
+            const header = document.querySelector('header');
+            if (window.scrollY > 50) {
+                header.classList.add('scrolled');
+            } else {
+                header.classList.remove('scrolled');
+            }
+        });
+    </script>
 </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\VideoController;
 use App\Http\Controllers\MateriController;
 use App\Http\Controllers\IsiMateriController;
+use App\Http\Controllers\KomunitasController;
 use App\Http\Controllers\QuizController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,6 +17,8 @@ Route::get('/video/{video}', [VideoController::class, 'show'])->name('video');
 Route::get('/materi/{materi}', [MateriController::class, 'show'])->name('materi');
 
 Route::get('/isi-materi/{isimateri}', [IsiMateriController::class, 'show'])->name('isi-materi');
+
+Route::get('/komunitas/{komunitas}', [KomunitasController::class, 'show'])->name('isi-komunitas');
 
 Route::get('komunitas', function () {
     return view('komunitas.index');
